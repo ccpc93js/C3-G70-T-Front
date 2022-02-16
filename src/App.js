eaa26cc0d9d887175d191fb21bd886f28f518f41
Mauricio Romero <mauricionromero@hotmail.com>
2022-02-16 13:53:14 -0300

    created component home

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from "react";
 import "rsuite/dist/rsuite.min.css";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import Home from "./Components/Home";
 
 // import { FlexboxGrid, Header } from "rsuite";
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/user" element={<h1>User</h1>} />
         <Route path="/login" element={<h1>Login</h1>} />
         <Route path="/*" element={<h1>Pagina no encontrada</h1>} />

0a43fe5b3adfd49891ca264c984eb9659e694542
Mauricio Romero <mauricionromero@hotmail.com>
2022-02-16 13:43:22 -0300

    completed login register service

--- src/app/services/users.js
+++ src/app/services/users.js
@@ -1,18 +1,26 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const usersApi = createApi({
-  reducerPath: 'usersApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://ingamers.api/' }),
+  reducerPath: "usersApi",
+  baseQuery: fetchBaseQuery({ baseUrl: "https://ingamers.api/" }),
   endpoints: (builder) => ({
-    login: builder.muration({
-      query: (credentials) => ({ url: 'auth/login', method: 'POST', body: credentials }),
+    login: builder.mutation({
+      query: (credentials) => ({
+        url: "auth/login",
+        method: "POST",
+        body: credentials,
+      }),
     }),
-    register: builder.muration({
-      query: (credentials) => ({ url: 'auth/register', method: 'POST', body: credentials }),
+    register: builder.mutation({
+      query: (credentials) => ({
+        url: "auth/register",
+        method: "POST",
+        body: credentials,
+      }),
     }),
-    
   }),
-})
+});
 
+export const { useLoginMutation, useRegisterMutation } = usersApi;
 
-export const { login } = usersApi
+export const { login } = usersApi;

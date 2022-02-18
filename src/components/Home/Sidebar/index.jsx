bc21c7969f23d20d3c68eb69e3392bac16d54812
Mauricio Romero <mauricionromero@hotmail.com>
2022-02-18 14:19:54 -0300

    errors fixed

---
+++ src/components/Home/Sidebar/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Sidebar() {
+  return (
+    <div>
+      Sidebar
+      <div>
+        <img src="https://i.pravatar.cc/150?img=13" alt="@juanmartin" />
+
+        <h3>Juan Martin</h3>
+        <p>@juanmartin</p>
+      </div>
+    </div>
+  );
+}

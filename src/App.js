2d2d8ce715ce37ad41cdd5879b7f7004dbf62e6c
Mauricio Romero <mauricionromero@hotmail.com>
2022-03-01 19:29:08 -0300

    styles fixes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "bootswatch/dist/vapor/bootstrap.min.css";
 import "./styles/styles.scss";
-import "./index.css";
 
 import { AppRouter } from "./routers/AppRouter";
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import "./styles/styles.scss";
+import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>

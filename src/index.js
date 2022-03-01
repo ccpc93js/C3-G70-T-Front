c5efab2653c896330fda45203851f7c4e1fb0ff1
Mauricio Romero <mauricionromero@hotmail.com>
2022-03-01 19:30:29 -0300

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { store } from "./app/store";
 import { Provider } from "react-redux";
 import "./styles/styles.scss";
-import "./index.css";
 
 ReactDOM.render(
   <Provider store={store}>

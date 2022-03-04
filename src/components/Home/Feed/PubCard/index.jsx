147d70f0b8420252e409cdd709d4490af648c3cd
Mauricio Romero <mauricionromero@hotmail.com>
2022-03-04 19:25:28 -0300

    las publicaciones ahora son leidas desde el servidor

--- src/components/Home/Feed/PubCard/index.jsx
+++ src/components/Home/Feed/PubCard/index.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Card,
   CardHeader,
@@ -16,14 +16,45 @@ import {
 import styles from "./PubCard.module.scss";
 
 export default function PubCard({ pub }) {
-  const { user, title, description, posted, image, likes } = pub;
-  console.log(user, title, description, posted, image);
+  const { avatar, id, title, description, posted, image, likes } = pub;
+  const [liked, setLiked] = useState(false);
+  const [likesCount, setLikesCount] = useState(likes);
+  const [disliked, setDisliked] = useState(false);
+
+  const handleLike = () => {
+    if (disliked) {
+      setLikesCount(likesCount + 2);
+      setDisliked(false);
+      setLiked(true);
+    } else if (liked) {
+      setLikesCount(likesCount - 1);
+      setLiked(false);
+    } else {
+      setLikesCount(likesCount + 1);
+      setLiked(true);
+    }
+  };
+
+  const handleDislike = () => {
+    if (liked) {
+      setLikesCount(likesCount - 2);
+      setLiked(false);
+      setDisliked(true);
+    } else if (disliked) {
+      setLikesCount(likesCount + 1);
+      setDisliked(false);
+    } else {
+      setLikesCount(likesCount - 1);
+      setDisliked(true);
+    }
+  };
+
   return (
     <Card body outline className={styles.card}>
       <CardHeader className={styles.cardHeader}>
-        <img src={user.avatar} alt={user.name} className="rounded-circle" />
+        <img src={avatar} alt={id} className="rounded-circle" />
         <div className={styles.cardHeader__info}>
-          <CardTitle>{user.name}</CardTitle>
+          <CardTitle>{id}</CardTitle>
           <CardText>{posted}</CardText>
         </div>
       </CardHeader>
@@ -33,11 +64,19 @@ export default function PubCard({ pub }) {
         <CardImg src={image} alt={title} />
       </CardBody>
       <CardBody className={styles.cardFooter}>
-        <FaComment className={styles.comment}/>
+        <FaComment className={styles.comment} />
         <CardText className={styles.likes}>
-          <FaArrowAltCircleDown className={styles.down} />
-          {likes}
-          <FaArrowAltCircleUp className={styles.up} />
+          <FaArrowAltCircleDown
+            className={styles.down}
+            onClick={handleDislike}
+            style={{ filter: disliked && "grayscale(100%)" }}
+          />
+          {likesCount}
+          <FaArrowAltCircleUp
+            className={styles.up}
+            onClick={handleLike}
+            style={{ filter: liked && "grayscale(100%)" }}
+          />
         </CardText>
         <FaShare className={styles.share} />
       </CardBody>
--- src/components/Home/Feed/index.jsx
+++ src/components/Home/Feed/index.jsx
@@ -1,50 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 import Share from "./../Share";
 import Card from "./PubCard";
 import styles from "./Feed.module.scss";
 
-const pubs = [
-  {
-    id: 1,
-    user: {
-      name: "John Doe",
-      avatar: "https://randomuser.me/api/portraits/med/men/8.jpg",
-    },
-    title: "Mira mi ultima jugada",
-    description:
-      "Estaba jugando con @juanito y de repente se me aparecio esto:",
-    image: "https://picsum.photos/id/1/200/300",
-    posted: "hace 2 horas",
-    likes: 654,
-  },
-  {
-    id: 2,
-    user: {
-      name: "Karen Smith",
-      avatar: "https://randomuser.me/api/portraits/med/women/8.jpg",
-    },
-    title: "Maravillosa jugada",
-    description: "Soy una persona muy maravillosa",
-    image: "https://picsum.photos/id/3/200/300",
-    posted: "hace 1 dia",
-    likes: 153,
-  },
-  {
-    id: 3,
-    user: {
-      name: "Robert Ran",
-      avatar: "https://randomuser.me/api/portraits/med/men/7.jpg",
-    },
-    title: "Anda la osa",
-    description: "Las cosas que pasan ",
-    image: "https://picsum.photos/id/2/200/300",
-    posted: "hace 1 dia",
-    likes: 153,
-  },
-  
-];
+// const pubs = [
+//   {
+//     id: 1,
+//     user: {
+//       name: "John Doe",
+//       avatar: "https://randomuser.me/api/portraits/med/men/8.jpg",
+//     },
+//     title: "Mira mi ultima jugada",
+//     description:
+//       "Estaba jugando con @juanito y de repente se me aparecio esto:",
+//     image: "https://picsum.photos/id/1/200/300",
+//     posted: "hace 2 horas",
+//     likes: 654,
+//   },
+//   {
+//     id: 2,
+//     user: {
+//       name: "Karen Smith",
+//       avatar: "https://randomuser.me/api/portraits/med/women/8.jpg",
+//     },
+//     title: "Maravillosa jugada",
+//     description: "Soy una persona muy maravillosa",
+//     image: "https://picsum.photos/id/3/200/300",
+//     posted: "hace 1 dia",
+//     likes: 153,
+//   },
+//   {
+//     id: 3,
+//     user: {
+//       name: "Robert Ran",
+//       avatar: "https://randomuser.me/api/portraits/med/men/7.jpg",
+//     },
+//     title: "Anda la osa",
+//     description: "Las cosas que pasan ",
+//     image: "https://picsum.photos/id/2/200/300",
+//     posted: "hace 1 dia",
+//     likes: 153,
+//   },
+
+// ];
 
 export default function Feed() {
+  const [pubs, setPubs] = useState([]);
+  const token = useSelector((state) => state.auth.token);
+
+  const getPubs = async () => {
+    const response = await fetch("https://api-ingamer.herokuapp.com/api/posts", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const data = await response.json();
+    setPubs(data);
+    console.log("Data: ",data);
+  };
+
+  useEffect(() => {
+    getPubs();
+  }, []); // eslint-disable-line
+
   return (
     <div className={styles.container}>
       <Share />

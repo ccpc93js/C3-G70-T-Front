4d6b3c8bcb003ef429a4a21e13c12ec0dbb512bf
Mauricio Romero <mauricionromero@hotmail.com>
2022-03-08 21:15:46 -0300

    data now is readed from backend

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -4,7 +4,7 @@ import { FcWebcam } from "react-icons/fc";
 export default function Avatar({ id }) {
   const { data } = useGetUserQuery(id);
 
-  if (!data) return <FcWebcam style={{ fontSize: "2.5rem" }} />;
+  if (!data?.avatar) return <FcWebcam style={{ fontSize: "2.5rem" }} />;
   return (
     <img src={data.avatar} className="rounded-circle" width="50" alt="avatar" />
   );

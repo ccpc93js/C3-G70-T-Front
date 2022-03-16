ba1645270c733cd4356df03013e6e8d199ff469b
unknown <djmlopez@hotmail.com>
2022-03-16 15:59:18 -0500

    update ENVIRONMENT .env

--- src/components/auth/client_secret_google.js
+++ src/components/auth/client_secret_google.js
@@ -1,24 +1,22 @@
-import { config } from 'dotenv';
-config();
 
 const {
-    CLIEN_ID,
-    PROJECT_ID,
-    AUTH_URI,
-    TOKEN_URI,
-    AUTH_PROVIDER_X509_CERT_URL,
-    CLIENT_SECRET,
-    REDIRECT_URIS,
-    JAVASCRIPT_ORIGINS
+    REACT_APP_CLIEN_ID,
+    REACT_APP_PROJECT_ID,
+    REACT_APP_AUTH_URI,
+    REACT_APP_TOKEN_URI,
+    REACT_APP_AUTH_PROVIDER_X509_CERT_URL,
+    REACT_APP_CLIENT_SECRET,
+    REACT_APP_REDIRECT_URIS,
+    REACT_APP_JAVASCRIPT_ORIGINS
 } = process.env
 
 export const client_secret_google = {
-    client_id: CLIEN_ID,
-    project_id: PROJECT_ID,
-    auth_uri: AUTH_URI,
-    token_uri: TOKEN_URI,
-    auth_provider_x509_cert_url: AUTH_PROVIDER_X509_CERT_URL,
-    client_secret: CLIENT_SECRET,
-    redirect_uris: REDIRECT_URIS,
-    javascript_origins: JAVASCRIPT_ORIGINS
+    client_id: REACT_APP_CLIEN_ID,
+    project_id: REACT_APP_PROJECT_ID,
+    auth_uri: REACT_APP_AUTH_URI,
+    token_uri: REACT_APP_TOKEN_URI,
+    auth_provider_x509_cert_url: REACT_APP_AUTH_PROVIDER_X509_CERT_URL,
+    client_secret: REACT_APP_CLIENT_SECRET,
+    redirect_uris: REACT_APP_REDIRECT_URIS,
+    javascript_origins: REACT_APP_JAVASCRIPT_ORIGINS
 }
